026cfd079dee58754ab71b898b3b87bd9e7584c9
George Griffin <george@Georges-MacBook-Pro.local>
2016-02-18 22:09:56 -0500

    Fixes payload

--- trollstick.py
+++ trollstick.py
@@ -1,7 +1,7 @@
-import os, urllib2
+import subprocess, urllib2
 
 image = urllib2.urlopen('https://raw.githubusercontent.com/tekromancr/trollstick/gh-pages/lol.jpg')
 with open("/tmp/lol.jpg", "wb") as f:
     f.write(image.read())
-command = '''osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/tmp/lol.jpg"'''
-os.system(command)
+command = '''osascript -e "tell application 'Finder' to set desktop picture to POSIX file '/tmp/lol.jpg'"'''
+subprocess.call(['osascript','-e','tell application "Finder" to set desktop picture to POSIX file "/tmp/lol.jpg"'])
