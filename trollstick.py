ba8173e3337a1a34a3c70d43cf938ea2b3402467
George Griffin <george@Georges-MacBook-Pro.local>
2016-02-18 21:36:48 -0500

    Payload 2

--- trollstick.py
+++ trollstick.py
@@ -1,6 +1,7 @@
 import os, urllib2
 
-image = urllib2.urlopen('http://tekromancr.github.io/trollstick/lol.jpg')
+image = urllib2.urlopen('https://raw.githubusercontent.com/tekromancr/trollstick/gh-pages/lol.jpg')
 with open("/tmp/lol.jpg", "wb") as f:
     f.write(image.read())
-
+command = '''osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/tmp/lol.jpg"'''
+os.system(command)
